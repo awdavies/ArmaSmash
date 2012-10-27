e93418194d952be12ce30044eb7886db34146502
Andrew Davies <andavies@cs.washington.edu>
2012-10-26 21:31:07 -0700

    Added some options for running the program.

--- run.sh
+++ run.sh
@@ -1,2 +1,35 @@
 #!/bin/bash
+TEST=
+
+usage() {
+  cat << EOF
+  usage: $0 options
+
+  This script runs the main program (for now).  It will also be running
+  tests soon enough.
+
+  OPTIONS:
+  -t      Run all tests.
+  -h      Prints this message.
+EOF
+}
+
+
+
+while getopts "t:h:r" OPTION
+do
+  case $OPTION in
+    h)
+      usage
+      exit
+      ;;
+    t)
+      TEST=1
+      ;;
+    ?)
+      usage
+      exit
+      ;;
+  esac
+done
 python2 ArmaSmash.py
