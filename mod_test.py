8e7d0f4450f161551604cfa376f9d936358e7ca6
Andrew Davies <andavies@cs.washington.edu>
2012-10-28 18:26:30 -0700

    Added more tests.

--- mod_test.py
+++ mod_test.py
@@ -13,15 +13,29 @@ class RsyncFolderCheck(unittest.TestCase):
     with self.assertRaises(Exception):
       self.mod = Mod("cow")
 
-  def test_existing_folder_and_rsync(self):
+  def test_existing_folder_rsync_version(self):
      '''
      Tests to see if the mod properly initializes if there exists
      a folder with a '.rsync' folder inside of it.
 
-     TODO: Need to check for "correct" rsync data.
      '''
-     self.mod = Mod("rsync_test_dir")
+     self.mod = Mod("rsync_test_dir/good_dir")
 
+  def test_existing_folder_rsync(self):
+    '''
+    Tests to see if an exception is thrown if there is not any data
+    for versioning within an rsync folder.
+    '''
+    with self.assertRaises(Exception):
+      self.mod = Mod("rsync_test_dir/bad_dir_no_version")
+
+  def test_existing_folder_version(self):
+    '''
+    Tests to see if an exception is thrown if there is not any data
+    for rsync within an rsync folder.
+    '''
+    with self.assertRaises(Exception):
+      self.mod = Mod("rsync_test_dir/bad_dir_no_rsync")
 
 if __name__ == "__main__":
   unittest.main()
